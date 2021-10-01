ea97a1567f51ed3b65733017022e6695002cf7bf
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-01 01:40:23 -0300

    aa

--- docker/prod.go
+++ docker/prod.go
@@ -58,13 +58,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	/*
+	
 	if imageBuild("/var/docker-images/filtros/Dockerfile", cli) {
-		ExampleCmd_StderrPipe()
+		//ExampleCmd_StderrPipe()
 	}else{
 		fmt.Println("ERROR CREAR IMAGEN")
 	}
-	*/
+	
 	pass := &MyHandler{ Conf: &Config{ Id: 8, Fecha: time.Now() }, cli: cli }
 
 	con := context.Background()
