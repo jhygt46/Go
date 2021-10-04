9f7b5d6980c11264bd56744e070aabb9020842c1
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-04 17:25:21 -0300

    aa

--- produccion/filtros/prod.go
+++ produccion/filtros/prod.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"fmt"
     "github.com/valyala/fasthttp"
 )
@@ -12,13 +13,17 @@ type MyHandler struct {
 func main() {
 
 	pass := &MyHandler {}
-
-	go func() {
-		fasthttp.ListenAndServe(":80", pass.HandleFastHTTP)
-	}()
-
+	fasthttp.ListenAndServe(":80", pass.HandleFastHTTP)
+	
 }
 
 func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+	time := time.Now()
 	fmt.Fprintf(ctx, "ERROR DDos");
+	printelaped(time, "HTTP")
+}
+
+func printelaped(start time.Time, str string){
+	elapsed := time.Since(start)
+	fmt.Printf("%s / Tiempo [%v]\n", str, elapsed)
 }
