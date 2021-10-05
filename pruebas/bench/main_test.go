bcdf5480ab3f2b15b6454f8fd2f6fed4d93b8e56
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-05 17:55:43 -0300

    aa

--- pruebas/bench/main_test.go
+++ pruebas/bench/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "os"
+    "io/ioutil"
     "bufio"
     "testing"
 	"fmt"
@@ -15,7 +16,7 @@ func main() {
 
 func BenchmarkCalculateA(b *testing.B) {
 
-    d1 := []byte("hello\ngo\n")
+    d1 := []byte{115, 111, 109, 101, 10}
     for i := 0; i < b.N; i++ {
         err := os.WriteFile("/var/Go/pruebas/utils/filtros/1/"+strconv.Itoa(i), d1, 0644)
         if err != nil {
@@ -28,54 +29,26 @@ func BenchmarkCalculateA(b *testing.B) {
 func BenchmarkCalculateB(b *testing.B) {
 
     d1 := []byte{115, 111, 109, 101, 10}
-
     for i := 0; i < b.N; i++ {
-        f, err := os.Create("/var/Go/pruebas/utils/filtros/2/"+strconv.Itoa(i))
-        if err != nil {
-            fmt.Println(err)
-        }
-        defer f.Close()
-        n2, err := f.Write(d1)
-        if err != nil {
-            fmt.Println(err)
-            fmt.Println(n2)
-        }
-        f.Sync()
+        ioutil.WriteFile("/var/Go/pruebas/utils/filtros/2/"+strconv.Itoa(i), d1, 0644);
     }
     
 }
-func BenchmarkCalculateC(b *testing.B) {
 
-    for i := 0; i < b.N; i++ {
-        f, err := os.Create("/var/Go/pruebas/utils/filtros/3/"+strconv.Itoa(i))
-        if err != nil {
-            fmt.Println(err)
-        }
-        defer f.Close()
-        n2, err := f.WriteString("writes\n")
-        if err != nil {
-            fmt.Println(err)
-            fmt.Println(n2)
-        }
-        f.Sync()
-    }
-    
-}
 func BenchmarkCalculateD(b *testing.B) {
 
     for i := 0; i < b.N; i++ {
-        f, err := os.Create("/var/Go/pruebas/utils/filtros/4/"+strconv.Itoa(i))
+        file, err := os.Create("/var/Go/pruebas/utils/filtros/3/"+strconv.Itoa(i))
         if err != nil {
             fmt.Println(err)
         }
-        defer f.Close()
-        w := bufio.NewWriter(f)
-        n4, err := w.WriteString("buffered\n")
+        writer := bufio.NewWriter(file)
+        bytesWritten, err := writer.WriteString("This is an example\n")
         if err != nil {
             fmt.Println(err)
-            fmt.Println(n4)
+            fmt.Println(bytesWritten)
         }
-        w.Flush()
+        writer.Flush()
     }
     
 }
@@ -90,15 +63,12 @@ func BenchmarkCalculateA(b *testing.B) {
         read_int32(byteNumber)
     }
 }
-
 func BenchmarkCalculateB(b *testing.B) {
     byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
     for i := 0; i < b.N; i++ {
         read_int32b(byteNumber)
     }
 }
-*/
-
 func read_int32(data []byte) int32 {
     var x int32
     for _, c := range data {
@@ -110,3 +80,4 @@ func read_int32b(data []byte) int {
     x, _ := strconv.Atoi(string(data))
     return x
 }
+*/
