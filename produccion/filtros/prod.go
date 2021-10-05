47ad2b41e9c981af7dfd51822a994e3b8298fdc9
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-05 16:13:57 -0300

    aa

--- produccion/filtros/prod.go
+++ produccion/filtros/prod.go
@@ -3,34 +3,62 @@ package main
 import (
 	"fmt"
 	"time"
+	"encoding/json"
     "github.com/valyala/fasthttp"
 )
 
 type MyHandler struct {
-	
+	minicache map[uint32]*Data
+}
+type Data struct {
+	C [] Campos `json:"C"`
+	E [] Evals `json:"E"`
+}
+type Campos struct {
+	T int `json:"T"`
+	N string `json:"N"`
+	V [] string `json:"V"`
+}
+type Evals struct {
+	T int `json:"T"`
+	N string `json:"N"`
 }
 
+
 func main() {
+
+	newpath := filepath.Join("/var/Go/pruebas/utils/filtros", "1")
+	err := os.MkdirAll(newpath, os.ModePerm)
 
 	pass := &MyHandler {}
 	fasthttp.ListenAndServe(":80", pass.HandleFastHTTP)
 	
 }
-func read_int32(data []byte) int32 {
-    var x int32
+func read_int32(data []byte) uint32 {
+    var x uint32
     for _, c := range data {
-        x = x * 10 + int32(c - '0')
+        x = x * 10 + uint32(c - '0')
     }
     return x
 }
 func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 
 	time := time.Now()
-	id := read_int32(ctx.QueryArgs().Peek("id"))
-	fmt.Fprintf(ctx, "HOLA");
+	
+	switch string(ctx.Path()) {
+	case "/filtro":
+		ctx.Response.Header.Set("Content-Type", "application/json")
+		id := read_int32(ctx.QueryArgs().Peek("id"))
+		if res, found := h.minicache[id]; found {
+			json.NewEncoder(ctx).Encode(res)
+		}else{
+
+		}
+	default:
+		ctx.Error("Not Found", fasthttp.StatusNotFound)
+	}
+
 	printelaped(time, "HTTP")
-	fmt.Println(id)
-	fmt.Println(ctx.Path())
 	
 
 }
--- pruebas/bench/main_test.go
+++ pruebas/bench/main_test.go
@@ -10,6 +10,45 @@ func main() {
     fmt.Println("Hello World")
 }
 
+
+func BenchmarkCalculateA(b *testing.B) {
+    /*
+    file := "catcuad/"+cat+"/"+cuad+".json"
+	jsonFile, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+    */
+    for i := 0; i < b.N; i++ {
+        
+    }
+
+}
+
+func BenchmarkCalculateB(b *testing.B) {
+
+}
+
+
+
+
+/*
+func BenchmarkCalculateA(b *testing.B) {
+    byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
+    for i := 0; i < b.N; i++ {
+        read_int32(byteNumber)
+    }
+}
+
+func BenchmarkCalculateB(b *testing.B) {
+    byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
+    for i := 0; i < b.N; i++ {
+        read_int32b(byteNumber)
+    }
+}
+*/
+
 func read_int32(data []byte) int32 {
     var x int32
     for _, c := range data {
@@ -21,23 +60,3 @@ func read_int32b(data []byte) int {
     x, _ := strconv.Atoi(string(data))
     return x
 }
-
-func BenchmarkCalculateA(b *testing.B) {
-
-    // 1254134
-    byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
-    for i := 0; i < b.N; i++ {
-        read_int32(byteNumber)
-    }
-
-}
-
-func BenchmarkCalculateB(b *testing.B) {
-
-    byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
-    for i := 0; i < b.N; i++ {
-        read_int32b(byteNumber)
-    }
-
-}
-
