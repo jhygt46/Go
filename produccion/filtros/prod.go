4c08e0fa1b05bcda1c0d1d61bbfbd4359f2f878b
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-08 00:54:31 -0300

    aa

--- produccion/filtros/prod.go
+++ produccion/filtros/prod.go
@@ -93,7 +93,7 @@ func getFolder(num int) string {
 	var c1 int = num / 1000000
 	var c2 int = num / 10000
 	var c3 int = num / 100
-	var c4 int = num
+	var c4 int = num % 100
 
 	//fmt.Printf("num[%v] c1[%v] c2[%v]", num, c1, c2)
 	return strconv.Itoa(c1)+"/"+strconv.Itoa(c2)+"/"+strconv.Itoa(c3)+"/"+strconv.Itoa(c4)
