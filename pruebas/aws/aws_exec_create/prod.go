5f2967d84f3bca930f0b75415ec54a1f9577aaa8
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-19 00:20:55 -0300

    aa

--- pruebas/aws/aws_exec_create/prod.go
+++ pruebas/aws/aws_exec_create/prod.go
@@ -17,18 +17,8 @@ func main(){
 
 func ExampleCmd_StdoutPipe() {
 	cmd := exec.Command("bash", "-c", "aws ec2 create-image --instance-id i-0f1afaf7e9156a147 --name 'My server' --description 'An AMI for my server'")
-	stdout, err := cmd.StdoutPipe()
+	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	if err := json.NewDecoder(stdout).Decode(&image); err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("ImageId is %s\n", image.ImageId)
 }
