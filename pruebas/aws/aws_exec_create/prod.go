ad828307956ef2d9a7b27cf9929e2d8d4ddc3dbf
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-19 00:32:48 -0300

    aa

--- pruebas/aws/aws_exec_create/prod.go
+++ pruebas/aws/aws_exec_create/prod.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	//"fmt"
+	"time"
 	"os/exec"
+	"context"
 	//"encoding/json"
 )
 
@@ -13,12 +15,23 @@ var image struct {
 
 func main(){
 	ExampleCmd_StdoutPipe()
+	ExampleCommandContext()
 }
 
 func ExampleCmd_StdoutPipe() {
-	cmd := exec.Command("bash", "-c", "aws ec2 create-image --instance-id i-0f1afaf7e9156a147 --name My server --description An AMI for my server")
+	cmd := exec.Command("bash", "-c", "aws ec2 create-image --instance-id i-07f96abb2dd303e22 --name Mys1 --description Ans1")
 	_, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func ExampleCommandContext() {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+
+	if err := exec.CommandContext(ctx, "aws ec2 create-image --instance-id i-07f96abb2dd303e22 --name Mys2 --description Ans2").Run(); err != nil {
+		// This will fail after 100 milliseconds. The 5 second sleep
+		// will be interrupted.
 	}
 }
