f830c5d3ee4ed069ee2751af693defba3f058262
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-21 22:20:18 -0300

    aa

---
+++ pruebas/aws/aws_instance_v2/prod.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+type EC2CreateInstanceAPI interface {
+	RunInstances(ctx context.Context, params *ec2.RunInstancesInput, optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
+	CreateTags(ctx context.Context, params *ec2.CreateTagsInput, optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+}
+
+func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunInstancesInput) (*ec2.RunInstancesOutput, error) {
+	return api.RunInstances(c, input)
+}
+
+func MakeTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
+	return api.CreateTags(c, input)
+}
+
+func main() {
+
+	instanceId := create_instance("ami-01fc81cf27918dbe6 ", "NomImg1", "DescImg1")
+	fmt.Println("imageId", instanceId)
+
+}
+
+func create_instance(ImageId string, TagName string, TagValue string) string {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+
+	client := ec2.NewFromConfig(cfg)
+
+	input := &ec2.RunInstancesInput{
+		ImageId:      &ImageId,
+		InstanceType: types.InstanceTypeT2Micro,
+		MinCount:     1,
+		MaxCount:     1,
+	}
+
+	result, err := MakeInstance(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error creating an instance:")
+		fmt.Println(err)
+		return ""
+	}
+
+	tagInput := &ec2.CreateTagsInput{
+		Resources: []string{*result.Instances[0].InstanceId},
+		Tags: []types.Tag{
+			{
+				Key:   name,
+				Value: value,
+			},
+		},
+	}
+
+	_, err = MakeTags(context.TODO(), client, tagInput)
+	if err != nil {
+		fmt.Println("Got an error tagging the instance:")
+		fmt.Println(err)
+		return ""
+	}
+
+	fmt.Println(result)
+	return *result.Instances[0].InstanceId
+	
+}
