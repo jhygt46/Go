804ae30862ce17e655eb7ef6043752df5c2cafb5
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-10-27 22:28:31 -0300

    aa

--- pruebas/bench/main_test.go
+++ pruebas/bench/main_test.go
@@ -5,34 +5,21 @@ import (
     "testing"
 	"fmt"
     //"strconv"
-    "crypto/rand"
-    "math/big"
+    //"crypto/rand"
+    //"math/big"
 )
+
+//https://gist.github.com/arsham/bbc93990d8e5c9b54128a3d88901ab90#file-go_cpu_memory_profiling_benchmarks-sh
+//go test -bench=. -benchmem
 
 func main() {
     fmt.Println("Hello World")
 }
 
-
 func BenchmarkCalculateA(b *testing.B) {
-
     for i := 0; i < b.N; i++ {
-        n, _ := rand.Int(rand.Reader, big.NewInt(1000000))
-        n.Int64()
+        
     }
-
-}
-
-func BenchmarkCalculateB(b *testing.B) {
-
-    d1 := []byte{115, 111, 109, 101, 10}
-    for i := 0; i < b.N; i++ {
-        err := os.WriteFile("/var/Go/pruebas/utils/filtros/1/"+strconv.Itoa(i), d1, 0644)
-        if err != nil {
-            fmt.Println(err)
-        }
-    }
-
 }
 
 
@@ -40,6 +27,22 @@ func BenchmarkCalculateB(b *testing.B) {
 
 
 /*
+func BenchmarkCalculateA(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        n, _ := rand.Int(rand.Reader, big.NewInt(1000000))
+        n.Int64()
+    }
+}
+
+func BenchmarkCalculateB(b *testing.B) {
+    d1 := []byte{115, 111, 109, 101, 10}
+    for i := 0; i < b.N; i++ {
+        err := os.WriteFile("/var/Go/pruebas/utils/filtros/1/"+strconv.Itoa(i), d1, 0644)
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+}
 func BenchmarkCalculateA(b *testing.B) {
     byteNumber := []byte{49, 50, 53, 52, 49, 51, 52}
     for i := 0; i < b.N; i++ {
---
+++ pruebas/cpu/prod.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	linuxproc "github.com/c9s/goprocinfo/linux"
+)
+
+func main() {
+
+	stat, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil {
+		log.Fatal("stat read fail")
+	}
+
+	for _, s := range stat.CPUStats {
+		// s.User
+		// s.Nice
+		// s.System
+		// s.Idle
+		// s.IOWait
+	}
+
+	// stat.CPUStatAll
+	// stat.CPUStats
+	// stat.Processes
+	// stat.BootTime
+
+}
+
