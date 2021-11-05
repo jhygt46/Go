bff4a6fd156020836fe311eee854c6a7aeaa6286
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-11-04 22:48:04 -0300

    aa

--- produccion/filtros/prod.go
+++ produccion/filtros/prod.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"log"
 	"time"
+	"strings"
 	"strconv"
 	"math/big"
 	"net/http"
@@ -185,6 +186,7 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 			defer jsonFiltro.Close()
 		}
 	case "/monitoring":
+		/*
 		stats := NewStats()
 		stats.GatherStats()
 		stats.PrintStats()
@@ -192,6 +194,16 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 		if err == nil {
 			fmt.Println(size)
 		}
+		*/
+		idle0, total0 := getCPUSample()
+		time.Sleep(3 * time.Second)
+		idle1, total1 := getCPUSample()
+
+		idleTicks := float64(idle1 - idle0)
+		totalTicks := float64(total1 - total0)
+		cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+
+		fmt.Printf("CPU usage is %f%% [busy: %f, total: %f]\n", cpuUsage, totalTicks-idleTicks, totalTicks)
 	case "/health":
 		fmt.Fprintf(ctx, "OK");
 	default:
@@ -478,6 +490,31 @@ func (s *stats) GatherStats() {
 		s.ProcCPUAvg = systemstat.GetProcCPUAverage(s.LastProcCPUSample, s.CurProcCPUSample, s.ProcUptime)
 	}
 	s.procCPUSampled = true
+}
+func getCPUSample() (idle, total uint64) {
+    contents, err := ioutil.ReadFile("/proc/stat")
+    if err != nil {
+        return
+    }
+    lines := strings.Split(string(contents), "\n")
+    for _, line := range(lines) {
+        fields := strings.Fields(line)
+        if fields[0] == "cpu" {
+            numFields := len(fields)
+            for i := 1; i < numFields; i++ {
+                val, err := strconv.ParseUint(fields[i], 10, 64)
+                if err != nil {
+                    fmt.Println("Error: ", i, fields[i], err)
+                }
+                total += val // tally up all the numbers to get total ticks
+                if i == 4 {  // idle is the 5th field in the cpu line
+                    idle = val
+                }
+            }
+            return
+        }
+    }
+    return
 }
 // MONITORING //
 
