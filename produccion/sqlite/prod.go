7773f9166c1e6a2c7702e4df5165ce8f483d0df5
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-11-26 22:59:42 -0300

    aa

--- produccion/sqlite/prod.go
+++ produccion/sqlite/prod.go
@@ -192,7 +192,8 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 	switch string(ctx.Path()) {
 	case "/get0":
 		
-		if res, found := h.Minicache[1]; found {
+		id := read_int64(ctx.QueryArgs().Peek("id"))
+		if res, found := h.Minicache[id]; found {
 			json.NewEncoder(ctx).Encode(res)
 		}else{
 			fmt.Println("NOT FOUND")
