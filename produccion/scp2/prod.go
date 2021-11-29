84bde155d5296b9a3ddc8152d9fb81ae28d8750f
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-11-29 00:53:49 -0300

    aa

--- produccion/scp2/prod.go
+++ produccion/scp2/prod.go
@@ -13,7 +13,7 @@ func main() {
 	scpClient, err := scp.NewClient("18.117.117.108:22", sshConf, &scp.ClientOption{})
 	defer scpClient.Close()
 
-	err = scpClient.CopyFileFromRemote("/var", "/var/dd.txt", &scp.FileTransferOption{})
+	err = scpClient.CopyFileFromRemote("/var/", "/var/dd.txt", &scp.FileTransferOption{})
 	if err != nil {
 		fmt.Println(err)
 	}
