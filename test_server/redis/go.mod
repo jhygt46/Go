09f64e0a4f9d0bb2bffea2a3f0237e755563f220
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-12-01 18:40:23 -0300

    aaa

--- test_server/redis/go.mod
+++ test_server/redis/go.mod
@@ -2,4 +2,7 @@ module example.com/prod
 
 go 1.16
 
-require github.com/gomodule/redigo v1.8.5 // indirect
+require (
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
+)
--- test_server/redis/prod.go
+++ test_server/redis/prod.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	// Import the redigo/redis package.
-	"github.com/gomodule/redigo/redis"
+    "context"
+    "github.com/go-redis/redis/v8"
 )
 
-func main() {
-	// Establish a connection to the Redis server listening on port
-	// 6379 of the local machine. 6379 is the default port, so unless
-	// you've already changed the Redis configuration file this should
-	// work.
-	conn, err := redis.Dial("tcp", "localhost:6379")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Importantly, use defer to ensure the connection is always
-	// properly closed before exiting the main() function.
-	defer conn.Close()
+var ctx = context.Background()
 
-	// Send our command across the connection. The first parameter to
-	// Do() is always the name of the Redis command (in this example
-	// HMSET), optionally followed by any necessary arguments (in this
-	// example the key, followed by the various hash fields and values).
-	_, err = conn.Do("HMSET", "album:2", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
-	if err != nil {
-		log.Fatal(err)
-	}
+func ExampleClient() {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
 
-	fmt.Println("Electric Ladyland added!")
+    err := rdb.Set(ctx, "key", "value", 0).Err()
+    if err != nil {
+        panic(err)
+    }
+
+    val, err := rdb.Get(ctx, "key").Result()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("key", val)
+
+    val2, err := rdb.Get(ctx, "key2").Result()
+    if err == redis.Nil {
+        fmt.Println("key2 does not exist")
+    } else if err != nil {
+        panic(err)
+    } else {
+        fmt.Println("key2", val2)
+    }
+    // Output: key value
+    // key2 does not exist
 }
