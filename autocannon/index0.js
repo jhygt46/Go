abef5d452e269b732cd715c7403e2e310cc481c3
diegomez <diego.gomez.bezmalinovic@gmail.com>
2021-12-01 14:27:51 -0300

    aa

--- autocannon/index0.js
+++ autocannon/index0.js
@@ -2,7 +2,7 @@
 const autocannon = require('autocannon');
 async function init(){
     const instance = autocannon({
-        url: 'http://18.117.135.40/get',
+        url: 'http://3.142.247.170/get',
         connections: 20,
         duration: 60,
         method: 'GET',
