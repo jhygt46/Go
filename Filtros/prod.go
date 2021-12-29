02110d77e8dc1bdaf84ad8771140c69e0f9cec46
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2021-12-29 19:17:45 -0300

    aa

--- Filtros/prod.go
+++ Filtros/prod.go
@@ -147,7 +147,8 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 			id := utils.Read_uint32(ctx.QueryArgs().Peek("id"))
 
 			if res, found := h.Cache[id]; found {
-				json.NewEncoder(ctx).Encode(res)
+				fmt.Fprintf(ctx, res)
+				//json.NewEncoder(ctx).Encode(res)
 				h.Count.Cache++
 			} else {
 				h.Count.Db++
