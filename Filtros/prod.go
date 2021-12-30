04764b5cd4f1abc42c65cc8d831f92d6f5a2de8b
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2021-12-30 17:59:16 -0300

    aa

--- Filtros/prod.go
+++ Filtros/prod.go
@@ -292,7 +292,7 @@ func (h *MyHandler) AddCache(file string) {
 	db, err := sql.Open("sqlite3", "/var/db/"+file)
 
 	if err == nil {
-		rows, err := db.Query("SELECT id, filtro FROM filtros LIMIT 200000")
+		rows, err := db.Query("SELECT id, filtro FROM filtros LIMIT 300000")
 		if err == nil {
 			defer rows.Close()
 			var id uint32
