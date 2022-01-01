6821eb42c5817dc050abbe6bf4a2639be5c7e58a
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-01 16:12:06 -0300

    aa

--- Pruebas/mapintdata/prod.go
+++ Pruebas/mapintdata/prod.go
@@ -7,9 +7,9 @@ import (
 	"strconv"
 	"time"
 
-	//"io/ioutil"
 	"crypto/rand"
 	"encoding/json"
+	"io/ioutil"
 	"path/filepath"
 
 	"github.com/valyala/fasthttp"
@@ -37,33 +37,33 @@ type MyHandler struct {
 	Total     int64      `json:"Total"`
 }
 type Minicache struct {
-	Cache map[int64]Data `json:"Cache"`
+	Cache map[int64]*Data `json:"Cache"`
 }
 
 func main() {
 
 	total := 350000
-	//escribir_file("/var/db1_test", total)
-	/*
-		cache := make(map[int64]*Data, total)
-		now := time.Now()
-		for i:=1; i<=total; i++ {
-			folderfile := getFolderFile64(random(int64(i)))
-			file, err := os.Open("/var/db1_test/"+folderfile)
-			if err != nil{
-				fmt.Println(err)
-			}
-			byteValue, err := ioutil.ReadAll(file)
-			file.Close()
-			data := Data{}
-			if err := json.Unmarshal(byteValue, &data); err == nil {
-				cache[int64(i)] = &data
-			}
+	escribir_file("/var/db1_test", total)
+
+	cache := make(map[int64]*Data, total)
+	now := time.Now()
+	for i := 1; i <= total; i++ {
+		folderfile := getFolderFile64(random(int64(i)))
+		file, err := os.Open("/var/db1_test/" + folderfile)
+		if err != nil {
+			fmt.Println(err)
 		}
-		elapsed := time.Since(now)
-		fmt.Printf("ADD FILES TO CACHE %v [%s] c/u total %v\n", total, time_cu(elapsed, total), elapsed)
-	*/
-	h := &MyHandler{Minicache: &Minicache{Cache: make(map[int64]Data, total)}, Total: int64(total)}
+		byteValue, err := ioutil.ReadAll(file)
+		file.Close()
+		data := Data{}
+		if err := json.Unmarshal(byteValue, &data); err == nil {
+			cache[int64(i)] = &data
+		}
+	}
+	elapsed := time.Since(now)
+	fmt.Printf("ADD FILES TO CACHE %v [%s] c/u total %v\n", total, time_cu(elapsed, total), elapsed)
+
+	h := &MyHandler{Minicache: &Minicache{Cache: cache}, Total: int64(total)}
 	fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
 
 }
