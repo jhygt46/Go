3d53de9da283ea9a9240f247e48230b8cd38d183
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-03 15:32:58 -0300

    aa

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ async function init(){
     const instance = autocannon({
         url: 'http://172.31.38.111/get0',
         connections: 20,
-        duration: 60,
+        duration: 10,
         method: 'GET',
-        pipelining: 8,
-        workers: 4
+        pipelining: 8
     });
     autocannon.track(instance, {renderProgressBar: true});
 }
