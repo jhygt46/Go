165bbb60f0e79be883474478e2c85406db2cd776
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-03 15:35:05 -0300

    aa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const autocannon = require('autocannon');
 async function init(){
     const instance = autocannon({
-        url: 'http://172.31.38.111/get0',
+        url: 'http://172.31.19.111/get0',
         connections: 20,
         duration: 10,
         method: 'GET',
