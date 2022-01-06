e6c5a6b29db5cc0c3f1418b9ba4a98d7c037299b
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-06 17:24:51 -0300

    aa

--- Pruebas/sqlitestringconcurrency/prod.go
+++ Pruebas/sqlitestringconcurrency/prod.go
@@ -12,16 +12,17 @@ import (
 )
 
 type MyHandler struct {
-	Dbs   *sql.DB `json:"Dbs"`
-	Total int64   `json:"Total"`
+	Total int64 `json:"Total"`
 }
+
+var DbGlobal *sql.DB
 
 func main() {
 
 	i, err := strconv.ParseInt(os.Args[1], 10, 64)
 	if err == nil {
 
-		sqlite, err := db.GetDbFiltroBytes("sFiltrodb0")
+		DbGlobal, err := db.GetDbFiltroBytes("sFiltrodb0")
 		//sqlite.SetMaxIdleConns(5)
 
 		if err == nil {
@@ -29,11 +30,11 @@ func main() {
 			filtro := db.Filtro{}
 			filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
 			filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
-			db.FiltroBytesInit(sqlite, filtro, i)
+			db.FiltroBytesInit(DbGlobal, filtro, i)
 
 		}
 
-		h := &MyHandler{Dbs: sqlite, Total: i}
+		h := &MyHandler{Total: i}
 		fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
 
 	}
@@ -46,7 +47,7 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 	switch string(ctx.Path()) {
 	case "/get0":
 
-		content, err := db.GetFiltroStringContent(h.Dbs, utils.Random(h.Total))
+		content, err := db.GetFiltroStringContent(DbGlobal, utils.Random(h.Total))
 		if err == nil {
 			fmt.Fprintf(ctx, content)
 		} else {
