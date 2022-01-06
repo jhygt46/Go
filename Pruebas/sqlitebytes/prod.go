9b88003b149c2a8728e4ac5030354c80278b40d6
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-06 01:14:29 -0300

    aa

--- Pruebas/sqlitebytes/prod.go
+++ Pruebas/sqlitebytes/prod.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"resource/db"
 	"resource/utils"
+	"strconv"
 
 	"github.com/valyala/fasthttp"
 )
@@ -16,19 +18,23 @@ type MyHandler struct {
 
 func main() {
 
-	var total int64 = 1000000
-	sqlite, err := db.GetDbFiltroBytes("bFiltrodb1")
+	i, err := strconv.ParseInt(os.Args[1], 10, 64)
 	if err == nil {
 
-		filtro := db.Filtro{}
-		filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
-		filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
-		db.FiltroBytesInit(sqlite, filtro, total)
+		sqlite, err := db.GetDbFiltroBytes("bFiltrodb1")
+		if err == nil {
+
+			filtro := db.Filtro{}
+			filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
+			filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
+			db.FiltroBytesInit(sqlite, filtro, i)
+
+		}
+
+		h := &MyHandler{Dbs: sqlite, Total: i}
+		fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
 
 	}
-
-	h := &MyHandler{Dbs: sqlite, Total: total}
-	fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
 
 }
 
