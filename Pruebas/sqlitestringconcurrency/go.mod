b81675e45fb620e41b48881d2368d2becedb97d7
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-06 19:17:18 -0300

    aa

--- Pruebas/sqlitestringconcurrency/go.mod
+++ Pruebas/sqlitestringconcurrency/go.mod
@@ -3,8 +3,15 @@ module example.com/prod
 go 1.16
 
 require (
+	crawshaw.io/sqlite v0.3.2 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bvinc/go-sqlite-lite v0.6.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	modernc.org/libc v1.13.2 // indirect
+	modernc.org/sqlite v1.14.3 // indirect
+	zombiezen.com/go/sqlite v0.8.0 // indirect
 )
--- Pruebas/sqlitestringconcurrency/prod.go
+++ Pruebas/sqlitestringconcurrency/prod.go
@@ -1,61 +1,42 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"os"
-	"resource/db"
-	"resource/utils"
-	"strconv"
+	"log"
+	"net/http"
 
-	"github.com/valyala/fasthttp"
+	"zombiezen.com/go/sqlite/sqlitex"
 )
 
-type MyHandler struct {
-	Total int64 `json:"Total"`
-}
+var dbpool *sqlitex.Pool
 
-var DbGlobal *sql.DB
-
+// Using a Pool to execute SQL in a concurrent HTTP handler.
 func main() {
-
-	i, err := strconv.ParseInt(os.Args[1], 10, 64)
-	if err == nil {
-
-		DbGlobal, err := db.GetDbFiltroBytes("sFiltrodb0")
-		//sqlite.SetMaxIdleConns(5)
-
-		if err == nil {
-
-			filtro := db.Filtro{}
-			filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
-			filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
-			db.FiltroBytesInit(DbGlobal, filtro, i)
-
-		}
-
-		h := &MyHandler{Total: i}
-		fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
-
+	var err error
+	dbpool, err = sqlitex.Open("C:/Allin/db/sFiltrodb0", 0, 10)
+	if err != nil {
+		log.Fatal(err)
 	}
-
+	http.HandleFunc("/", handle)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
-func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
-
-	ctx.Response.Header.Set("Content-Type", "application/json")
-	switch string(ctx.Path()) {
-	case "/get0":
-
-		content, err := db.GetFiltroStringContent(DbGlobal, utils.Random(h.Total))
-		if err == nil {
-			fmt.Fprintf(ctx, content)
-		} else {
-			ctx.Error("Not Found", fasthttp.StatusNotFound)
-		}
-
-	default:
-		ctx.Error("Not Found", fasthttp.StatusNotFound)
+func handle(w http.ResponseWriter, r *http.Request) {
+	conn := dbpool.Get(r.Context())
+	if conn == nil {
+		return
 	}
-
+	defer dbpool.Put(conn)
+	stmt := conn.Prep("SELECT filtro FROM filtros WHERE id = $id")
+	stmt.SetText("$id", "1")
+	for {
+		if hasRow, err := stmt.Step(); err != nil {
+			// ... handle error
+		} else if !hasRow {
+			break
+		}
+		foo := stmt.GetText("filtro")
+		// ... use foo
+		fmt.Fprintln(w, foo)
+	}
 }
