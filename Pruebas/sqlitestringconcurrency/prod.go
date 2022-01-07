ae6e8cdf502269013454973b8bc9d13e90a8f265
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-06 22:24:19 -0300

    aa

--- Pruebas/sqlitestringconcurrency/prod.go
+++ Pruebas/sqlitestringconcurrency/prod.go
@@ -2,6 +2,89 @@ package main
 
 import (
 	"fmt"
+	"resource/utils"
+
+	"github.com/bvinc/go-sqlite-lite/sqlite3"
+	"github.com/valyala/fasthttp"
+)
+
+type MyHandler struct {
+	Total int64         `json:"Total"`
+	Dbs   *sqlite3.Conn `json:"Dbs"`
+	Stmt  *sqlite3.Stmt `json:"Stmt"`
+}
+
+func main() {
+
+	conn, err := sqlite3.Open("file:C:/Allin/db/sFiltrodb0?cache=shared&mode=ro")
+	if err != nil {
+		fmt.Println("Err1", err)
+	}
+
+	stmt, err := conn.Prepare("SELECT filtro FROM filtros WHERE id=?")
+	if err != nil {
+		fmt.Println("Err2", err)
+	}
+
+	defer stmt.Close()
+	defer conn.Close()
+
+	h := &MyHandler{Dbs: conn, Stmt: stmt, Total: 1000000}
+	fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
+
+}
+
+func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	switch string(ctx.Path()) {
+	case "/get0":
+
+		err := h.Stmt.Bind(utils.Random(h.Total))
+		_, err = h.Stmt.Step()
+		check(err)
+		var filtro string
+		err = h.Stmt.Scan(&filtro)
+		check(err)
+		err = h.Stmt.Reset()
+		check(err)
+		fmt.Fprintf(ctx, filtro)
+
+		/*
+			stmt, err := h.Dbs.Prepare(`SELECT filtro FROM filtros WHERE id = ?`, 18)
+			check(err)
+			defer stmt.Close()
+
+			for {
+				hasRow, err := stmt.Step()
+				check(err)
+				if !hasRow {
+					break
+				}
+
+				var filtro string
+				err = stmt.Scan(&filtro)
+				check(err)
+				fmt.Fprintf(ctx, filtro)
+			}
+		*/
+	default:
+		ctx.Error("Not Found", fasthttp.StatusNotFound)
+	}
+
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+/*
+package main
+
+import (
+	"fmt"
 	"log"
 	"net/http"
 	"resource/utils"
@@ -42,3 +125,4 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, foo)
 	}
 }
+*/
