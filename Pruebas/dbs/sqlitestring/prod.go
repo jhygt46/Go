8618b2ab93ac9bcd2e69c66b0ba8beeedd94f468
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-08 00:33:00 -0300

    aa

--- Pruebas/dbs/sqlitestring/prod.go
+++ Pruebas/dbs/sqlitestring/prod.go
@@ -8,7 +8,6 @@ import (
 	"resource/utils"
 	"strconv"
 
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/valyala/fasthttp"
 )
 
@@ -22,41 +21,19 @@ func main() {
 	i, err := strconv.ParseInt(os.Args[1], 10, 64)
 	if err == nil {
 
-		dbs, err := sql.Open("sqlite3", "/var/db/sFiltrodb0")
+		sqlite, err := db.GetDbFiltroString("sFiltrodb0")
+		//sqlite.SetMaxIdleConns(5)
+
 		if err == nil {
-			/*
-				stmt1, err := dbs.Prepare("CREATE TABLE IF NOT EXISTS autocomplete (filtro TEXT NOT NULL, id INTEGER NOT NULL, PRIMARY KEY (id))")
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println("DB autocomplete CREATED")
-				}
-				stmt1.Exec()
-			*/
-		}
-		/*
+
 			filtro := db.Filtro{}
 			filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
 			filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
+			db.FiltroStringInit(sqlite, filtro, i)
 
-			db.FiltroStringInit(dbs, filtro, 999990)
-			db.FiltroStringInit2(dbs, filtro, 999990)
-		*/
-		/*
-			sqlite, err := db.GetDbFiltroString("sFiltrodb0")
-			//sqlite.SetMaxIdleConns(5)
+		}
 
-			if err == nil {
-
-				filtro := db.Filtro{}
-				filtro.C = []db.Campos{db.Campos{T: 1, N: "Procesador", V: []string{"X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71", "X15", "IntelC3", "Amd71"}}, db.Campos{T: 1, N: "Pantalla", V: []string{"4", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5", "4.5", "5.5"}}, db.Campos{T: 1, N: "Memoria", V: []string{"2GB", "4GB", "8GB", "16GB", "32GB", "64GB", "128GB"}}, db.Campos{T: 1, N: "Marca", V: []string{"Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia", "Samsung", "Motorola", "Nokia"}}}
-				filtro.E = []db.Evals{db.Evals{T: 1, N: "Buena"}, db.Evals{T: 1, N: "Nelson"}, db.Evals{T: 1, N: "Hola"}, db.Evals{T: 1, N: "Mundo"}}
-				//db.FiltroStringInit(sqlite, filtro, i/2)
-				//db.FiltroStringInit2(sqlite, filtro, i/2)
-
-			}
-		*/
-		h := &MyHandler{Dbs: dbs, Total: i}
+		h := &MyHandler{Dbs: sqlite, Total: i}
 		fasthttp.ListenAndServe(":80", h.HandleFastHTTP)
 
 	}
